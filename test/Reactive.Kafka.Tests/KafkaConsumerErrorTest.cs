5cb164d0db6016297988832671a0c03ccdbf0720
Bruno Moura <contato@brunomoura.dev>
2022-01-21 17:38:19 -0300

    KafkaConsumerError class test

---
+++ test/Reactive.Kafka.Tests/KafkaConsumerErrorTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Reactive.Kafka.Errors;
+using Reactive.Kafka.Exceptions;
+using Xunit;
+
+namespace Reactive.Kafka.Tests;
+
+public class KafkaConsumerErrorTest
+{
+    [Fact]
+    public void PlacesExceptionAndChecksPropertyValue()
+    {
+        // Arrange
+        const string expected = "Exception generated";
+        var value = new Exception("Exception generated");
+        var value2 = new KafkaConsumerException("Exception generated");
+        
+        // Act
+        var consumerError = new KafkaConsumerError(value);
+        var consumerError2 = new KafkaConsumerError(value2);
+
+        // Assert
+        Assert.NotNull(consumerError.Exception);
+        Assert.True(consumerError.KafkaMessage == expected);
+        Assert.NotNull(consumerError2.Exception);
+        Assert.True(consumerError2.KafkaMessage == expected);
+    }
+}
